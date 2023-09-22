1bd600ab1428b0bd2744ebb34aceeba9a40329d3
wryxmq <wulongkj@protonmail.com>
2023-09-22 17:10:21 +0800

    20230922
    1.将不再显示连接

---
+++ js/index.js
@@ -0,0 +1,53 @@
+// JSON数据
+var data = [
+  {
+    "name": "百度",
+    "url": "https://www.baidu.com",
+    "tags": ["百度", "搜索引擎","中国"]
+  },
+  {
+    "name": "谷歌",
+    "url": "https://www.google.com",
+    "tags": ["谷歌", "搜索引擎","美国"]
+  },{
+	"name":"F搜",
+	"url":"https://fsoufsou.com",
+	"tags":["F搜","搜索引擎"]
+  },{
+    "name": "导航网站",
+    "url": "http://wulongkj.github.io",
+    "tags": ["导航", "导航网站","中国"]
+  },{
+	"name":"MediaFire",
+	"url":"https://mediafire.com",
+	"tags":["MediaFire","网盘","美国","免费"]
+  },{
+	"name":"文叔叔",
+	"url":"https://www.wenshushu.cn",
+	"tags":["文叔叔","网盘","中国","免费","文件分享"]
+  },{
+	  "name":"Temp Mail",
+	  "url":"https://etempmail.com/zh",
+	  "tags":["临时邮箱","临时","邮箱","mail"]
+  },{
+	  "name":"百度翻译",
+	  "url":"https://fanyi.baidu.com",
+	  "tags":["百度翻译","翻译","中国"]
+  },{
+	  "name":"大巫",
+	  "url":"https://dawu.world",
+	  "tags":["免费openAi","免费","AI"]
+  },{
+	  "name":"谷歌地图",
+	  "url":"https://maps.google.com",
+	  "tags":["谷歌地图","地图","谷歌","美国"]
+  },{
+	  "name":"vivo官网",
+	  "url":"https://www.vivo.com.cn/service/authenticityCheck/index",
+	  "tags":["谷歌地图","地图","谷歌","美国"]
+  },{
+	  "name":"oppo官网",
+	  "url":"https://support.oppo.com/cn/check",
+	  "tags":["谷歌地图","地图","谷歌","美国"]
+  }
+];
